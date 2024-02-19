bfd428eb146b9e14c88d33cdc605307d41a7a526
omega-jean <jeanalex1405@gmail.com>
2024-02-19 13:23:47 +0000

    Hello, printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,18 @@
+/*
+ * 5-printf.c
+ * author : omega-jean 8897@holbertonstudents.com
+ */
+
+#include <stdio.h>
+
+/**
+ * main - Prints "with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: Always 0.
+ */
+
+int main (void)
+{
+printf("with proper grammar, but the outcome is a piece of art," + \n);
+return (0);
+}
