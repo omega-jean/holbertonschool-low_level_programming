cf5778fc2ce4e149fd19126cbaea85c2eda0a627
omega-jean <jeanalex1405@gmail.com>
2024-02-19 13:27:26 +0000

    Hello, printf

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -11,9 +11,9 @@
  * Return: Always 0.
  */
 
-int main (void)
+int main(void)
 {
-printf ("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art,");
 
 return (0);
 }
