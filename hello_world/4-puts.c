797b362a564cb1f4d00f61f9d38558e54635b224
omega-jean <jeanalex1405@gmail.com>
2024-02-19 10:43:58 +0000

    Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,2 +1,6 @@
-#!/bin/bash
-gcc -c 4-puts.c | puts('"Programming is like building a multilingual puzzle' \n) return 0
+#include <stdio.h>
+
+int main () {
+	puts('"Programming is like building a multilingual puzzle' + \n);
+	return 0;
+}
