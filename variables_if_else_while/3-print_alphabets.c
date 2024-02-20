0f98db2953658762c356022c65fe144663525e21
omega-jean <jeanalex1405@gmail.com>
2024-02-20 13:47:56 +0000

    alphABET

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,36 @@
+/*
+ * File: 3-print_alphabets.c
+ * Auth: omega-jean 8897@holbertonstudents.com
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - "This program prints the alphabet in lowercase,
+ *         followed by a new line
+ *
+ * Return: Always 0.
+ */
+
+
+int main(void)
+{
+int n = 97;
+int m = 65;
+
+while (n <= 122)
+{
+putchar(n);
+n++;
+}
+while (m <= 90)
+{
+putchar(n);
+n++;
+}
+putchar('\n);
+
+return (0);
+}
