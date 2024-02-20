9d98359cdf9bc94b203c8a2dee0528cf3bfa35e8
omega-jean <jeanalex1405@gmail.com>
2024-02-20 14:10:15 +0000

    alphABET

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -17,11 +17,11 @@
 
 int main(void)
 {
-char = n;
-for (n = '97'; n <= '122'; n++)
-putchar(n);
-for (n = '65'; n <= '90'; n++)
-putchar(n);
+char = c;
+for (c = 'a'; n <= 'z'; c++)
+putchar(c);
+for (c = 'A'; c <= 'Z'; c++)
+putchar(c);
 putchar('\n');
 
 return (0);
