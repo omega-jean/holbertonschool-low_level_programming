d7b512e28eaee960a45b92d225211454b4173450
omega-jean <jeanalex1405@gmail.com>
2024-02-20 13:35:07 +0000

    alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,26 @@
+/*
+ * File: 2-print_alphabet.c
+ * Auth: omega-jean 8897@holbertonstudents.com
+ */
+
+#include <stdio.h>
+
+/**
+ * main - "This program prints the alphabet in lowercase, 
+ *         followed by a new line
+ *
+ * Return: Always 0.
+ */
+
+
+int main(void)
+{
+int n=97
+while (n <= 122;)
+putchar (n);
+
+putchar ('\n')
+n++
+
+return (0);
+}
