ea11dea346c975ad92e8af1a538d5b7f34dcccf8
omega-jean <jeanalex1405@gmail.com>
2024-02-20 10:32:58 +0000

    The last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -30,7 +30,7 @@ if ((n % 10) > 5)
 printf("the string and %d is greater than 5\n", n, n % 10);
 else if ((n % 10) == 0)
 printf("%d the string and %d is 0\n", n, n % 10);
-else if ((n % 10)) < 6 && ((n % 10) != 0)
+else if (((n % 10) < 6) && ((n % 10) != 0))
 printf("%d the string and %d is less than 6 and not 0\n", n, n % 10);
 
 return (0);
