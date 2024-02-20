3c405963135b9b4f1610467ddbf879efd5822db3
omega-jean <jeanalex1405@gmail.com>
2024-02-20 10:39:05 +0000

    The last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -27,11 +27,11 @@ srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes there */
 if ((n % 10) > 5)
-printf(" the string and %d is greater than 5\n", n, n % 10);
+printf(" the string and %d is greater than 5\n", n % 10);
 else if ((n % 10) == 0)
-printf("the string and %d is 0\n", n, n % 10);
+printf("the string and %d is 0\n", n % 10);
 else if (((n % 10) < 6) && ((n % 10) != 0))
-printf(" the string and %d is less than 6 and not 0\n", n, n % 10);
+printf(" the string and %d is less than 6 and not 0\n", n % 10);
 
 return (0);
 }
