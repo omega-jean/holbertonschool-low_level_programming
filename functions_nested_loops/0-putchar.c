9d92ad260fc9ff33eb4a1620393340797b7dcbb2
omega-jean <jeanalex1405@gmail.com>
2024-02-21 10:25:33 +0000

    putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -3,4 +3,24 @@
  * Auth: omega-jean 8897@holbertonstudents.com
  */
 
-printf (_putchar)
+#include "main.h"
+
+/**
+ * main - Print _putchar
+ * Return: Always 0
+ *
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
