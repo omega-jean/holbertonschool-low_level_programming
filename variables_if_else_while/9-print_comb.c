8ae50a27ab9aa56a3f8267a9658518c505b3ca2a
omega-jean <jeanalex1405@gmail.com>
2024-02-21 09:09:27 +0000

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,31 @@
+/*
+ * File: 9-print_comb.c
+ * Auth: omega-jean 8897@holbertonstudents.com
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - "program that prints all possible combinations of single-digit
+ *numbers.
+ *
+ * Return: Always 0.
+ */
+
+
+int main(void)
+{
+int n = 48;
+
+while (n <= 57)
+{
+putchar(n);
+putchar(44);
+putchar('\n');
+n++;
+}
+
+return (0);
+}
