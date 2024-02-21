560ee35c3f07c0b6f25916ebe63aabda45a0e264
omega-jean <jeanalex1405@gmail.com>
2024-02-21 09:04:00 +0000

    Hexadecimal

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -8,8 +8,8 @@
 #include <stdio.h>
 
 /**
- * main - "This program prints all base 16 numbers in lowercase, 
- * 		followed by a new line.
+ * main - "This program prints all base 16 numbers in lowercase,
+ * followed by a new line.
  *
  * Return: Always 0.
  */
