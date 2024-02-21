e05bbc8365dc67d11e48859092552af87b9477f5
omega-jean <jeanalex1405@gmail.com>
2024-02-21 14:32:40 +0000

    islower" git commit -m islower
    git commit -m 'islower'
    git commit -m 'islower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -7,6 +7,12 @@
 #include <ctype.h>
 #include <unistd.h>
 
+/**
+ * main - "fonction qui vérifie les caractères minuscule
+ * Return: Always 0
+ *
+ */
+
 int _islower(int c)
 {
 if (c >= 97 && c <= 122)
