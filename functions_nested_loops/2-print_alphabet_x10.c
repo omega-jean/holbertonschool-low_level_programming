0ed6c21c1d77c34be0b323f940e38fd73361e0ae
omega-jean <jeanalex1405@gmail.com>
2024-02-21 13:56:13 +0000

    10 x alphabet

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+/*
+ * File: 2-print_alphabet_x10.c
+ * Auth: omega-jean
+ */
+
+#include "main.h"
+
+void print_alphabet_x10(void);
+{
+char n;
+int c = 0;
+while(c <= 9)
+{
+for (n = 'a'; n <= 'z'; n++)
+_putchar(n);
+
+_putchar('\n');
+}
+}
