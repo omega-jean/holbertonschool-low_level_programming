dfb1650a3d7d2d749f1ccb2b0459dd2453b47cec
omega-jean <jeanalex1405@gmail.com>
2024-02-21 08:21:49 +0000

    Smile in the mirror

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,28 @@
+/*
+ * File: 7-print_tebahpla.c
+ * Auth: omega-jean 8897@holbertonstudents.com
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - "This program prints the alphabet upside down in lowercase
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+int n = 122;
+
+while(n <= 97)
+{
+putchar(n);
+n++
+}
+putchar('\n');
+
+return(0)
+}
