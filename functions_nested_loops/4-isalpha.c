9f929da726c7c5ddb6850a1cf71bc7d7efcd0c27
omega-jean <jeanalex1405@gmail.com>
2024-02-22 08:48:48 +0000

    isalpha

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+/*
+ * File: 4-isalpha.c
+ * Auth: omega-jean 8897@holbertonstudents.com
+ */
+
+#include "main.h"
+
+int _isalpha(int c)
+{
+if (c >= 97 && c <= 122 && c >= 65 && c <= 90)
+return (1);
+else
+return (0);
+}
