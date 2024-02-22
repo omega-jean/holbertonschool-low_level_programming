7e25db197ffa2fbe554cab03ce8b5773a65e569d
omega-jean <jeanalex1405@gmail.com>
2024-02-22 08:57:25 +0000

    isalpha

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -5,6 +5,13 @@
 
 #include "main.h"
 
+/**
+  * _isalpha - Check for alphabetic characters
+  * @c: The character to be checked
+  *
+  * Return: 1 for alphabetic characters ans 0 for anything else
+  */
+
 int _isalpha(int c)
 {
 if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
