8e34c1448fd3fc3a9abc2e781d62d715100e1d1d
omega-jean <jeanalex1405@gmail.com>
2024-02-22 13:49:52 +0000

    lest_digit

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -5,6 +5,21 @@
 
 #include "main.h"
 
+/**
+ * _last_digit - function to return the last digit
+ * @n: The integer to be computed.
+ *
+ * Returns: the value of the last digit
+ */
+
 int print_last_digit(int n)
 {
-
+if (n < 0)
+{
+return 10 - (n % 10);
+}
+else
+{
+return n % 10
+}
+}
