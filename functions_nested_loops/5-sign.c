34f64e08f25f284abfc7fbd46c7cdd8f4f635607
omega-jean <jeanalex1405@gmail.com>
2024-02-22 12:58:36 +0000

    Sign

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -5,6 +5,15 @@
 
 #include "main.h"
 
+/**
+  * print_sign - Prints the sign of a number.
+  * @c: The character to be checked
+  *
+  * Return: 1 and prints + if n is greater than zero
+  *         0 and prints 0 if n is zero
+  *         -1 and prints - if n is less than zero
+  */
+
 int print_sign(int n)
 {
 	if (n > 0)
