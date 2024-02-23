5b198c51a9a94ed9105468509046d482433c4d93
omega-jean <jeanalex1405@gmail.com>
2024-02-23 13:25:36 +0000

    Inventing is a combination of brains and materials. The more brains you use, the less material you need

---
+++ variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+/*
+ * File: 100-print_comb3.c
+ * Auth: omega-jean 8897@holbertonstudents.com
+ */
+
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+int main(void)
+{
+int a;
+int b;
+int c;
+for (a = '0'; a <= 9; a++)
+{
+for (b = '0'; b <= 9; b++)
+{
+for (c = '0'; c <= 9; c++)
+{
+putchar(a);
+putchar(b);
+putchar(c);
+putchar(',');
+putchar(' ');
+}
+}
+}
