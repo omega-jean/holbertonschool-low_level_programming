8b10900eb237f1ea31cc25d8ea6bb204d095da14
omega-jean <jeanalex1405@gmail.com>
2024-02-23 10:01:54 +0000

    98 Battery Street, the OG

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -20,12 +20,12 @@ if (n <=98)
 {
 while (n < 98)
 printf("%d, ", n++);
-printf("%d,\n", n);
+printf("%d\n", n);
 }
 else
 {
 while (n > 98)
 printf("%d, ", n--);
-printf("%d,\n", n);
+printf("%d\n", n);
 }
 }
