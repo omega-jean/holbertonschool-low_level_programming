3abe39593df633f16741f7e8216d225157fe3785
omega-jean <jeanalex1405@gmail.com>
2024-02-23 09:51:02 +0000

    98 Battery Street, the OG

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,31 @@
+/*
+ * File: 11-print_to_98.c
+ * Auth: omega-jean 8897@holbertonstudents.com
+ */
+
+#include "main.h"
+
+/**
+* print_to_98 - Counts up to 98
+*
+* @n : Number of Departures
+*
+* Return: return result
+*/
+
+void print_to_98(int n)
+{
+if (n == 98)
+return (98);
+{
+while (n < 98)
+printf("%d, ", n++);
+printf("%d,\n", n++);
+}
+else
+{
+while (n > 98)
+printf("%d, ", n++);
+printf("%d,\n", n--);
+}
+}
