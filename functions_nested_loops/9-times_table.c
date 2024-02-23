8c3708a341fcce5d2821daed69d5779f60a28634
omega-jean <jeanalex1405@gmail.com>
2024-02-23 08:55:45 +0000

    Learn your times table

---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,34 @@
+/*
+ * File: 9-times_table.c
+ * Auth: omega-jean 8897@holbertonstudents.com
+ */
+
+#include "main.h"
+
+/**
+* times_table - Printed multiplication tables
+*
+* Return: returns 0
+*/
+
+void times_table(void)
+{
+int mult;
+int num;
+int result;
+for (num = 0; <= 9; num++)
+{
+_putchar('0');
+{
+for (mult = 0; <=9; mult++)
+_putchar(',');
+_putchar(' ');
+result = num * mult;
+if (result <= 9)
+_putchar(' ');
+else
+_putchar(result / 10 + '0');
+_putchar(result % 10 + '0');
+}
+_putchar('\n');
+}
