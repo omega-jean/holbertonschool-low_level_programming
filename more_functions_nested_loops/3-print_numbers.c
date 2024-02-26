0defb4215f7c5b246812ddf9b8aed7f2a7690ed1
omega-jean <jeanalex1405@gmail.com>
2024-02-26 10:35:23 +0000

    The numbers speak for themselves

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -7,8 +7,9 @@
 
 /**
   * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
-  * @n : number
+  * @n : The integer to be computed.
   *
+  *  return : The absolute value of the integer
   */
 
 void print_numbers(void)
