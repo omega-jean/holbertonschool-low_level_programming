b1ca4f18e389c7d539bbd7d700426f80dc7a5b34
omega-jean <jeanalex1405@gmail.com>
2024-02-26 13:57:11 +0000

    I feel like I am diagonally parked in a parallel universe

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,15 @@
+/*
+ * File: 7-print_diagonal.c
+ * Auth: omega-jean 8897@holbertonstudents.com
+ */
+
+#include "main.h"
+
+void print_diagonal(int n)
+{
+while (n-- > 0)
+{
+_putchar('\');
+}
+_putchar('\n');
+}
