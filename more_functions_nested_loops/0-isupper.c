e0c179f5a87a69dac1dafc0160628ec93ca8d166
omega-jean <jeanalex1405@gmail.com>
2024-02-26 09:52:47 +0000

    isupper

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -3,4 +3,19 @@
  * Auth: omega-jean 8897@holbertonstudents.com
  */
 
+#include "main.h"
 
+/**
+  * _isupper - Checks for the presence of uppercase characters
+  * @c: The character to be checked
+  *
+  * Return: 1 for uppercase characters or 0 for everything else
+  */
+
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+return (1);
+else
+return (0);
+}
