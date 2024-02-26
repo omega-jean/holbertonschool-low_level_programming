6fe6a730fb9df4b32999abacea31fe4b603dfdd0
omega-jean <jeanalex1405@gmail.com>
2024-02-26 10:48:06 +0000

    I believe in numbers and signs

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+/*
+ * File: 4-print_most_numbers.c
+ * Auth: omega-jean 8897@holbertonstudents.com
+ */
+
+#include "main.h"
+
+{
+int n = 48;
+while (n <= 57)
+{
+if (n = 52)
+_putchar(n+1);
+n++;
+}
+_putchar('\n');
+}
