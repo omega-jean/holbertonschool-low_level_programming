f339e9e8f330f1f0051ed4f0a4bf81a5aca250af
omega-jean <jeanalex1405@gmail.com>
2024-02-26 10:32:35 +0000

    The numbers speak for themselves

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+/*
+ * File: 3-print_numbers.c
+ * Auth: omega-jean 8897@holbertonstudents.com
+ */
+
+#include "main.h"
+
+/**
+  * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
+  * @n : number
+  *
+  */
+
+void print_numbers(void)
+{
+int n = 48;
+while (n <= 57)
+{
+_putchar(n);
+n++
+}
+_putchar("\n");
+}
