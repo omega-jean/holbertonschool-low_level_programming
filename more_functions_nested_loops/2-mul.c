7ff574636314211e4830038b1b547e7f73227090
omega-jean <jeanalex1405@gmail.com>
2024-02-26 10:15:10 +0000

    Collaboration is multiplication

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+/*
+ * File: 2-mul.c
+ * Auth: omega-jean 8897@holbertonstudents.com
+ */
+
+#include "main.h"
+
+/**
+  * mul - multiply 2 number
+  * @a: number one
+  * @b: number two
+  *
+  * Return: one number by one number
+  */
+
+int mul(int a, int b)
+{
+return (a * b);
+}
