0d1bb0d74db784aee2c13a630b8b4177a97798c1
omega-jean <jeanalex1405@gmail.com>
2024-02-26 14:23:03 +0000

    You are so much sunshine in every square inch

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,27 @@
+/*
+ * File: 8-print_square.c
+ * Auth: omega-jean 8897@holbertonstudents.com
+ */
+
+#include "main.h"
+
+/**
+  * print_square - function that prints a square, followed by a new line.
+  *
+  * size: That's the size of the square
+  *
+  * return: The absolute value of the integer
+  */
+
+void print_square(int size)
+{
+int a, b;
+for (a = 0; a < num1; a++)
+{
+for (b = 0; b < size; b++)
+{
+printf("#");
+}
+printf("\n");
+}
+}
