90436bc9716a30a55bf1f8d06484cc7330eef010
omega-jean <jeanalex1405@gmail.com>
2024-02-26 10:50:14 +0000

    I believe in numbers and signs

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -5,6 +5,13 @@
 
 #include "main.h"
 
+/**
+  * print_numbers - Print numbers from zero to nine followed by a line
+  *
+  *
+  *  return: The absolute value of the integer
+  */
+
 void print_numbers(void)
 {
 int n = 48;
