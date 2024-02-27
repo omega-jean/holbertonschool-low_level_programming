bead1e66f17c5d56f4c1ed25c22d9201d8541835
omega-jean <jeanalex1405@gmail.com>
2024-02-27 13:52:35 +0000

    I do not fear computers. I fear the lack of them

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  * _puts - function that prints a string, 
+  * followed by a new line, to stdout.
+  * @str: string
+  *
+  * Return: the size of the string
+  */
+
+void _puts(char *str)
+{
+while (*str)
+_putchar(*str++);
+_putchar('\n');
+}
