876ea63e7759941819a91f65ac45321288cf8556
omega-jean <jeanalex1405@gmail.com>
2024-02-27 09:02:35 +0000

    Fizz-Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -19,12 +19,12 @@ int main(void)
 int n = 1;
 while (n <= 100)
 {
-if ((n % 3) == 0)
-printf("Fizz");
+if (((n % 3) == 0) && ((n % 5) == 0))
+printf("FizzBuzz");
 else if ((n % 5) == 0)
 printf("Buzz");
-else if (((n % 3) == 0) && ((n % 5) == 0))
-printf("FizzBuzz");
+else if ((n % 3) == 0)
+printf("Fizz");
 else
 printf("%d", n);
 if (n < 100)
