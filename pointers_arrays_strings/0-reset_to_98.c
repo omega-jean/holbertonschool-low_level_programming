df2b3fa58335d6451fdffc382d58e417785ae0e3
omega-jean <jeanalex1405@gmail.com>
2024-02-27 10:08:52 +0000

    0. 98 Battery st.

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -6,7 +6,7 @@
 #include "main.h"
 
 /**
-  * reset_to_98 - function that takes a pointer to an int as parameter 
+  * reset_to_98 - function that takes a pointer to an int as parameter
   * and updates the value it points to to 98.
   * @n: pointer n is the value of the number
   *
