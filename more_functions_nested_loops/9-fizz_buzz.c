8b2dafef112d743501f12d9fb994d9c9ce63b4cb
omega-jean <jeanalex1405@gmail.com>
2024-02-27 08:38:15 +0000

    Fizz-Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -5,5 +5,38 @@
 
 #include "main.h"
 
+/**
+  * main - program that prints the numbers from 1 to 100
+  *
+  *
+  * Return: fizz if it's multiple of 3, buzz if it's multiple of 5 
+  * 	    and fizzbuzz if it's both
+  */
+
 int main(void)
 {
+int n;
+while (n >= 1 && n <= 100)
+_putchar(n);
+{
+if (n % 3)
+{
+_putchar("Fizz");
+}
+else if
+{
+for (n % 5)
+{
+_putchar("Buzz");
+}
+else
+{
+for (n % 3 && n % 5)
+{
+_putchar("FizzBuzz")
+}
+}
+}
+}
+_putchar('\n');
+}
