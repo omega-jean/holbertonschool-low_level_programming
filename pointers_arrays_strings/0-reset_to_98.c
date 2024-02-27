d4a415e01c8dc34238e333fa7a68e6ab88353727
omega-jean <jeanalex1405@gmail.com>
2024-02-27 10:05:06 +0000

    0. 98 Battery st.

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+/*
+ * File: 0-reset_to_98.c
+ * Auth: omega-jean 8897@holbertonstudents.com
+ */
+
+#include "main.h"
+
+/**
+  * main - program that prints the numbers from 1 to 100
+  * @*n: pointer n is the value of the number
+  *
+  * Return: always 0
+  */
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
