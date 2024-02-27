42d13be1b1ea6449a20634d325f9a9df43275693
omega-jean <jeanalex1405@gmail.com>
2024-02-27 09:29:52 +0000

    test

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -16,12 +16,16 @@
 void print_triangle(int size)
 {
 int a;
+int b;
 if (size <= 0)
 _putchar('\n');
-else
+else if
 {
 for (a = 0; a < size; a++)
 {
+for (b = 0, b < (size - (b +1); b++)
+_putchar(32);
+else
 _putchar(35);
 }
 _putchar('\n');
