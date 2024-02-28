8149d4e51b889931a775df1605cddef5d3147ce1
omega-jean <jeanalex1405@gmail.com>
2024-02-28 08:32:23 +0000

    I can only go one way. Ive not got a reverse gear

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,12 @@
 #include "main.h"
 
+/**
+  * print_rev - function that prints a string, in reverse.
+  * @s: string
+  *
+  * Return: the size of the string
+  */
+
 void print_rev(char *s)
 {
 int len;
