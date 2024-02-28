c8e745cd15118c5cec31bd5e7c3d3233cdf939c9
omega-jean <jeanalex1405@gmail.com>
2024-02-28 08:36:58 +0000

    all

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -9,15 +9,11 @@
 
 void print_rev(char *s)
 {
-int len;
-while (*s)
+int len = 0;
+int i = 0;
+while (s[len])
 len++;
-{
-int i;
-for (i = len-1; i >= 0; i++)
-{
+for (i = len - 1; i >= 0; i--)
 _putchar(s[i]);
-}
-}
 _putchar('\n');
 }
