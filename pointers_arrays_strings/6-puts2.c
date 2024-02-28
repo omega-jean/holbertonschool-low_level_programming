d75b12560fef450a0460c3a706619b0e7967d0b7
omega-jean <jeanalex1405@gmail.com>
2024-02-28 09:25:50 +0000

    puts2

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,11 +9,8 @@
 
 void puts2(char *str)
 {
-int i = 0;
-int len = 0;
-while (str[len])
-len++;
-for (i = len -1; i >= 0; i+=2)
-_putchar(str[i]);
+{
+while (*str)
+_putchar(*str+=2);
 _putchar('\n');
 }
