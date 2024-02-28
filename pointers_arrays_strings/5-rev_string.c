5548e2a0691bc2b551c8201fde23eb12223f61dd
omega-jean <jeanalex1405@gmail.com>
2024-02-28 08:47:04 +0000

    everses a string

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+void rev_string(char *s)
+{
+char str[100], rev[100];
+int a = strlen(str);;
+int b = 0;
+int c = 0;
+rev[t] = '\0';
+for (b = a -1; b >= 0; b--)
+{
+rev[c++] = str[b];
+}
+rev[b] = '\0';
+return (0);
+}
