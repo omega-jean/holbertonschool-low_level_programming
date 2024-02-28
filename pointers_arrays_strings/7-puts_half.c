a29fe70673380038dd3ccc87cd6ad7eb9980af3b
omega-jean <jeanalex1405@gmail.com>
2024-02-28 13:47:16 +0000

    fix

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -9,14 +9,14 @@
 
 void puts_half(char *str)
 {
-int a, b;
+int a, n;
 int longi = 0;
 for (a = 0; str[a] != '\0'; a++)
 longi++;
-b = (longi / 2);
+n = (longi / 2);
 if ((longi % 2) == 1)
-b = ((longi + 1) / 2);
-for (a = b; str[a] != '\0'; a++)
+n = ((longi + 1) / 2);
+for (a = n; str[a] != '\0'; a++)
 _putchar(str[a]);
 _putchar('\n');
 }
