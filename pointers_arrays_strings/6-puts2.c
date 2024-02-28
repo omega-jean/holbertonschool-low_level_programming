8a8f115545aadb1b5aba404d25a443cc905761be
omega-jean <jeanalex1405@gmail.com>
2024-02-28 09:23:55 +0000

    puts2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+  * puts2 - function that prints a string, in reverse.
+  * @str: string
+  *
+  * Return: the size of the string
+  */
+
+void puts2(char *str)
+{
+int i = 0;
+int len = 0;
+while (s[len])
+len++;
+for (i = len -1; i >= 0; i+=2)
+_putchar(s[i]);
+_putchar('\n');
+}
