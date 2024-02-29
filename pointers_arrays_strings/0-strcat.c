1f462215316e800ad745917759822cee66fd4917
omega-jean <jeanalex1405@gmail.com>
2024-02-29 13:23:41 +0000

    strcat

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-  * _strcat - function that concatenates two strings. 
+  * _strcat - function that concatenates two strings.
   * @dest: destination
   * @src: source
   *
