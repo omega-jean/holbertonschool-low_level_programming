e1792899c13c43b831a75958440236eb48a0327a
omega-jean <jeanalex1405@gmail.com>
2024-02-29 14:23:54 +0000

    strncat

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * _strncat - function that concatenates two strings.
+  * @dest: destination
+  * @src: source
+  * @n: it will use at most n bytes from src
+  *
+  * Return: destination
+  */
+
+char *_strncat(char *dest, char *src, int n)
