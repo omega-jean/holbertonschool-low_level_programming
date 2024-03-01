3698492924e0f8e3f1d1734041bd7cc162ab9bc5
omega-jean <jeanalex1405@gmail.com>
2024-03-01 14:01:37 +0000

    Always look up

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
-  * string_toupper - function that changes all lowercase letters of a string to uppercase.
+  * string_toupper - function that changes all lowercase
+  * letters of a string to uppercase.
   * @str: string
   *
   * Return: dif
