494400db120be6d412f62034b204111ae91a098c
omega-jean <jeanalex1405@gmail.com>
2024-03-01 13:59:32 +0000

    Always look up

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -2,16 +2,16 @@
 
 /**
   * string_toupper - function that changes all lowercase letters of a string to uppercase.
-  * @str: string
+  * @a: string
   *
   * Return: dif
   */
 
-char *string_toupper(char *str)
+char *string_toupper(char *a)
 {
 int i;
 for (i = 0; str[i] != '\0'; i++)
-if (str[i] >= 97 && str[i] <= 122)
-str[i] = str[i] - 32;
-return (str);
+if (a[i] >= 97 && a[i] <= 122)
+a[i] = a[i] - 32;
+return (a);
 }
