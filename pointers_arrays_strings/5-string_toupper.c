9b702bef0bbc557ce7d738ee53e4f32b21ef3f9b
omega-jean <jeanalex1405@gmail.com>
2024-03-01 13:58:06 +0000

    Always look up

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -11,9 +11,7 @@ char *string_toupper(char *str)
 {
 int i;
 for (i = 0; str[i] != '\0'; i++)
-{
 if (str[i] >= 97 && str[i] <= 122)
-str[i] = (int)str[i] - 32;
-}
+str[i] = str[i] - 32;
 return (str);
 }
