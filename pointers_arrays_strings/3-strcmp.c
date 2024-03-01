f41544cd2568e296307c1d5f7d0f4974e0f35f27
omega-jean <jeanalex1405@gmail.com>
2024-03-01 10:19:49 +0000

    strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -11,21 +11,7 @@
 int _strcmp(char *s1, char *s2)
 {
 int i = 0;
-int dif = 0;
-if (s1[i] == '\0' && s2[i] == '\n')
-
-else if (s1[i] == '\0')
-{
-dif = s2[i];
-else if (s2[i] == '\0')
-{
-dif = s1[i];
-else (s1[i] = '\0' != s2[i] = '\n')
-{
-dif = (s1[i] - s2[i])
-}
-}
-}
+while (s1[i] - s2[i] == 0 && s1[i] != '\0')
 i++;
-return dif;
+return (s1[i] - s2[i]);
 }
