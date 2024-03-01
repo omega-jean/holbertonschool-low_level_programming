dedc3ca509d9d88e32896511916a97ab8c8b8431
omega-jean <jeanalex1405@gmail.com>
2024-03-01 09:24:08 +0000

    strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -11,13 +11,10 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
-char* start = dest;
-while (*src && n--)
-{
-*dest = *src;
-dest++;
-src++;
-}
-*dest = '\n';
-return start;
+int i;
+for (i = 0; i < n && src[i] != '\0'; i++)
+dest[i] = src[i];
+for ( ; i < n; i++)
+dest[i] = '\0';
+return (dest);
 }
