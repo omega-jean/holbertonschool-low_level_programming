8b9b1887ea6a6a74b2a68ffba826ca8608985944
omega-jean <jeanalex1405@gmail.com>
2024-03-04 09:54:37 +0000

    test

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+  * _memset - function that fills memory with a constant byte.
+  * @dest: destination
+  * @src: source
+  * @n: number of time
+  *
+  * Return: always 0
+  */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+
+}
