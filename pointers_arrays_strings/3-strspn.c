c0971b302ef11ce2286d329aec11ce78c534edb0
omega-jean <jeanalex1405@gmail.com>
2024-03-04 14:34:28 +0000

    task 3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+  * _strspn - function that locates a character in a string.
+  * @s: string
+  * @accept: accept
+  *
+  * Return: a pointer to the byte in s that matches one of the bytes in accept
+  */
+
+char *_strpbrk(char *s, char *accept)
+{
+
+}
