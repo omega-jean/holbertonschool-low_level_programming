abb210a581a291cac7c687a375f84eb41c6d7a0b
omega-jean <jeanalex1405@gmail.com>
2024-03-06 13:42:44 +0000

    task 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -14,14 +14,14 @@ return (check_prime(n, 1));
 }
 
 /**
-  * str - Check if number is prime
+  * _str - Check if number is prime
   * @n: the number to be checked
   * @i: the iteration times
   *
   * Return: 1 for prime or 0 composite
   */
 
-int str(int n, int i)
+int _str(int n, int i)
 {
 if (n <= 1)
 return (0);
@@ -29,5 +29,5 @@ if (n % i == 0 && i > 1)
 return (0);
 if ((n / i) < i)
 return (1);
-return (str(n, i + 1));
+return (_str(n, i + 1));
 }
