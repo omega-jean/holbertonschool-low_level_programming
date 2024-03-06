0714273afe8a8105051852a6962a492e348a1564
omega-jean <jeanalex1405@gmail.com>
2024-03-06 09:07:21 +0000

    task 1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -8,11 +8,11 @@
 
 void _print_rev_recursion(char *s)
 {
-if (*s == '\0')
+if (*s > '\0')
 _putchar('\n');
 else
 {
 _putchar(s[0]);
-_print_rev_recursion(s - 1);
+_print_rev_recursion(s + 1);
 }
 }
