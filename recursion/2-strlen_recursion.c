629cd13a66f930fc3f531a80a1494bfb5f33a71d
omega-jean <jeanalex1405@gmail.com>
2024-03-06 09:37:01 +0000

    task 2

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -6,15 +6,9 @@
 *Return:returns void
 */
 
-void _puts_recursion(char *s)
+int _strlen_recursion(char *s)
 {
-if (*s == '\0')
-{
-_putchar('\n');
-}
-else
-{
-_putchar(*s);
-_puts_recursion(s + 1);
-}
+if (*s != '\0')
+return (1 + _strlen_recursion(s + 1));
+return (0);
 }
