321353057c53a82667d607461706e4840af03b56
omega-jean <jeanalex1405@gmail.com>
2024-03-06 09:00:19 +0000

    task 1

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+* _print_rev_recursion - 
+* @s:character to be printed
+
+* Return:returns void
+*/
+
+void _print_rev_recursion(char *s)
+{
+if (*s == '\0')
+_putchar('\n');
+else
+{
+_putchar(s[0]);
+_puts_recursion(s - 1);
+}
+}
