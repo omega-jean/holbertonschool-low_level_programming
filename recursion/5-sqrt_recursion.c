68e4f25e5c0b8b49eb52be3f53b8de52f46e008d
omega-jean <jeanalex1405@gmail.com>
2024-03-07 13:26:30 +0000

    task 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -18,7 +18,22 @@ else
 return (operation(n, 1);
 }
 
+/**
+ * operation - make the operation to find the root.
+ *
+ * @n: number
+ * @root: root of n
+ *
+ * Return: If n does not have a natural square root,
+ * the function should return -1
+ */
 
 int operation(int n, int root)
 {
+if (root * root == 0)
+return (root);
+else if (root * root < n)
+return (operation (root + 1));
+else
+return (-1)
 }
