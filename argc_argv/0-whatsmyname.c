064e1fd8ef2ab03fdc4e66671cfd436f30d57dd9
omega-jean <jeanalex1405@gmail.com>
2024-03-07 14:21:54 +0000

    task 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -10,8 +10,8 @@
  * Return: Always 0.
  */
 
-int main (int argc, char *argv[])
+int main (int argc __attribute__((unused)), char *argv[])
 {
-printf ("%s\n",argv[0]);
+printf("%s\n", argv[0]);
 return (0);
 }
