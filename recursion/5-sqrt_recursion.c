aac33ba01289b7e07b73d4cf2dc3f6acb665ecad
omega-jean <jeanalex1405@gmail.com>
2024-03-07 13:31:49 +0000

    task 5

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -1,24 +1,6 @@
 #include "main.h"
 
 /**
- * _sqrt_recursion - function that returns the natural
- * square root of a number.
- *
- * @n: number
- *
- * Return: If n does not have a natural square root,
- * the function should return -1
- */
-
-int _sqrt_recursion(int n)
-{
-if (n < 0)
-return (-1);
-else
-return (operation(n, 1));
-}
-
-/**
  * operation - make the operation to find the root.
  *
  * @n: number
@@ -36,4 +18,22 @@ else if (root * root < n)
 return (operation(root + 1));
 else
 return (-1);
+}
+
+/**
+ * _sqrt_recursion - function that returns the natural
+ * square root of a number.
+ *
+ * @n: number
+ *
+ * Return: If n does not have a natural square root,
+ * the function should return -1
+ */
+
+int _sqrt_recursion(int n)
+{
+if (n < 0)
+return (-1);
+else
+return (operation(n, 1));
 }
