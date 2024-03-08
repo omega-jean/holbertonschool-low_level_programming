6617736ac5f26c0bd9243aa19518fd53224ac543
omega-jean <jeanalex1405@gmail.com>
2024-03-08 13:08:37 +0000

    all

--- argc_argv/main.h
+++ argc_argv/main.h
@@ -4,7 +4,7 @@
 int _putchar(char n);
 int main(int argc __attribute__((unused)), char *argv[]);
 int main(int argc, char *argv[] __attribute__((unused)));
-
+int main(int argc, char *argv[]);
 
 
 
