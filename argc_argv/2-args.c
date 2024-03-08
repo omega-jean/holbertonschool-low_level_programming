3d043fd713ffc637af976333ce31c3043e2db7fc
omega-jean <jeanalex1405@gmail.com>
2024-03-08 11:51:30 +0000

    task 2

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,19 +1,20 @@
-#include "main.h"
 #include <stdio.h>
+#include "main.h"
 
 /**
- * main - program that prints all arguments it receives.
+ * main - prints all arguments it receives
+ * @argc: number of arguments
+ * @argv: array of arguments
  *
- * @argc: number of arguments passed to the function.
- * @argv: argument vector of pointers to strings.
- *
- * Return: Always 0.
+ * Return: Always 0 (Success)
  */
 
 int main(int argc, char *argv[])
 {
 int i;
-for(i = 0; i < argc; i++)
-printf(%s\n, i, argv[i]);	
-return 0;
+for (i = 0; i < argc; i++)
+{
+printf("%s\n", argv[i]);
+}
+return (0);
 }
