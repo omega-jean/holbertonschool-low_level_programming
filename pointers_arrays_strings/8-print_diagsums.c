a75ec3f6c8e7e452d5605d71f7d461f477bbfb3e
omega-jean <jeanalex1405@gmail.com>
2024-03-08 21:41:34 +0000

    task 8

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -7,21 +7,21 @@
 */
 void print_diagsums(int *a, int size)
 {
-int i, sum1 = 0, sum2 = 0;
+	int i, sum1 = 0, sum2 = 0;
 
-for (i = 0; i < size; i++)
-{
-sum1 += a[i];
-a += size;
-}
+	for (i = 0; i < size; i++)
+	{
+		sum1 += a[i];
+		a += size;
+	}
 
-a -= size;
+	a -= size;
 
-for (i = 0; i < size; i++)
-{
-sum2 += a[i];
-a -= size;
-}
+	for (i = 0; i < size; i++)
+	{
+		sum2 += a[i];
+		a -= size;
+	}
 
-printf("%d, %d\n", sum1, sum2);
+	printf("%d, %d\n", sum1, sum2);
 }
