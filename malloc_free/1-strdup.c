bb2a7c7a0fa89740ae6476dae204bc69a4a19921
omega-jean <jeanalex1405@gmail.com>
2024-03-11 13:59:29 +0000

    task 1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -3,8 +3,8 @@
 #include <stdio.h>
 
 /**
- * _strdup - function that returns a pointer to a newly allocated space in memory,
- * which contains a copy of the string given as a parameter.
+ * _strdup - function that returns a pointer to a newly allocated space in
+ * memory, which contains a copy of the string given as a parameter.
  *
  * @str: string
  *
