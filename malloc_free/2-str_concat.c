4d42e881338142c784601871d03c3a2cabcf1e98
omega-jean <jeanalex1405@gmail.com>
2024-03-11 14:25:46 +0000

    task 2

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * _str_concat - function that concatenates two strings.
+ *
+ * @s1: string1
+ * @s2: string2
+ *
+ * Return: always 0
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+
