d7e653f0f0a13835a85118f819d4547fab258f9a
omega-jean <jeanalex1405@gmail.com>
2024-03-11 13:56:20 +0000

    task 1

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -21,7 +21,7 @@ if (str == 0)
 return (NULL);
 while (*str++)
 strlen++;
-s = malloc(sizeof(char) * strlen)
+s = malloc(sizeof(char) * strlen);
 if (s == 0)
 return (NULL);
 for (i = 0; i < strlen; i++)
