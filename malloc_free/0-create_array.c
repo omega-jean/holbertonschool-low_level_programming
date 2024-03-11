ab0f2991baf03ed5662cd39711dadb9429f6b696
omega-jean <jeanalex1405@gmail.com>
2024-03-11 10:47:01 +0000

    task 0

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -14,14 +14,12 @@
 
 char *create_array(unsigned int size, char c)
 {
+unsigned int i
 if (size == 0)
 return ('\0');
-char *array = malloc(size);
-if (array == '\0')
-{
-return ('\0');
-}
-for (unsigned int i = 0; i < size; i++)
+char *array;
+array = malloc(sizeof(char) * size)
+for (i = 0; i < size; i++)
 {
 array[i] = c;
 }
