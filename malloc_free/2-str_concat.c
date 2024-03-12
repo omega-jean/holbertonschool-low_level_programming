551377cb426e6b5ecde2c7d8c19da98f21615e9e
omega-jean <jeanalex1405@gmail.com>
2024-03-12 08:23:37 +0000

    task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -5,12 +5,32 @@
 /**
  * _str_concat - function that concatenates two strings.
  *
- * @s1: string1
- * @s2: string2
+ * @s1: pointer 1
+ * @s2: pointer 2
  *
  * Return: always 0
  */
 
 char *str_concat(char *s1, char *s2)
 {
-
+unsigned int i, j, k, l;
+char *s;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
+for (i = 0; s1[i] != '\0'; i++)
+for (j = 0; s2[j] != '\0'; j++)
+s = malloc(sizeof(char) * (i + j));
+if (s == NULL)
+{
+free(s):
+return (NULL):
+}
+for (k = 0; k < i; k++)
+strout[k] = s1[k];
+l = j;
+for (j = 0; j <= l; k++, j++)
+s[k] = s2[j];
+return (s);
+}
