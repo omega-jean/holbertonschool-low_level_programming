1763bab117e10e57746db030879c1399d15f20a3
omega-jean <jeanalex1405@gmail.com>
2024-03-12 14:20:58 +0000

    task 1

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -14,26 +14,27 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-unsigned int i, j, k, l;
 char *s;
-if (s1 == 0)
-return NULL;
-if (s2 == 0)
-return NULL;
+unsigned int i, j, k, l;
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 for (i = 0; s1[i] != '\0'; i++)
 ;
 for (j = 0; s2[j] != '\0'; j++)
 ;
-if (n > s2)
-n = s2;
-s = malloc(s + 1);
+if (n > j)
+n = j;
+lsout = i + n;
+s = malloc(k + 1);
 if (s == NULL)
 return (NULL);
 for (l = 0; l < k; l++)
 if (l < i)
-s[l] = s1[l]
+s[l] = s1[l];
 else
-s[l] = s2[l - s1];
+s[l] = s2[l - i];
 s[l] = '\0';
 return (s);
 }
