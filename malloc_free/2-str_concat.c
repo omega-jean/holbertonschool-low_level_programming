da02b92711f1e96d67921ae45401c7a501de6732
omega-jean <jeanalex1405@gmail.com>
2024-03-12 08:34:29 +0000

    task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -13,7 +13,7 @@
 
 char *str_concat(char *s1, char *s2)
 {
-unsigned int i, j, k, limit;
+unsigned int i, j, k, l;
 char *s;
 if (s1 == NULL)
 s1 = "";
@@ -21,7 +21,7 @@ if (s2 == NULL)
 s2 = "";
 for (i = 0; s1[i] != '\0'; i++)
 for (j = 0; s2[j] != '\0'; j++)
-s = malloc(sizeof(char) * (i + j));
+s = malloc(sizeof(char) * (i + j + 1));
 if (s == NULL)
 {
 free(s);
@@ -29,8 +29,8 @@ return (NULL);
 }
 for (k = 0; k < i; k++)
 s[k] = s1[k];
-limit = j;
-for (j = 0; j <= limit; k++, j++)
+l = j;
+for (j = 0; j <= l; k++, j++)
 s[k] = s2[j];
 return (s);
 }
