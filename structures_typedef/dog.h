4f243822bd2287e0a11c264e9f3355f97a0be84d
omega-jean <jeanalex1405@gmail.com>
2024-03-13 13:34:45 +0000

    dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,7 +8,7 @@
  * @owner: owner of the dog.
  */
 
-dog_t
+struct dog
 {
 char *name;
 float age;
@@ -17,7 +17,7 @@ char *owner;
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
+void print_dog(struct dog *d);
 
 
 
