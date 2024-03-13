666c94f55d719b34da8fc4aeee5e2f6896e4ab0a
omega-jean <jeanalex1405@gmail.com>
2024-03-13 13:46:18 +0000

    task 2

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,21 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * print_dog - function that prints a struct dog
+ *
+ * @d: array
+ *
+ * return: always 0
+ */
+
+
+void print_dog(struct dog *d);
+{
+if (d == NULL)
+return;
+printf("Name: %s\n");
+printf("Age: %d\n" );
+printf("Owner: %s\n");
+}
