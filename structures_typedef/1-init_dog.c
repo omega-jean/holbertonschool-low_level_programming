33e413fc1b494fdba695a16e10c03648fa5ec04b
omega-jean <jeanalex1405@gmail.com>
2024-03-13 13:27:18 +0000

    tesk 1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,5 +1,4 @@
 #include "dog.h"
-#include <stdio.h>
 
 /**
  * init_dog - function that initialize a variable of type struct dog
@@ -14,12 +13,10 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-if (d == NULL)
+if (d)
 {
-printf("error");
-return;
-}
 d->name = name;
 d->age = age;
 d->owner = owner;
+}
 }
