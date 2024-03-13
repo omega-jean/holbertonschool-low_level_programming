439a35cea152cc4de88176ede4c5192d0cce65e8
omega-jean <jeanalex1405@gmail.com>
2024-03-13 08:38:42 +0000

    task 2

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -14,10 +14,10 @@
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 unsigned int i;
-char *s
+char *s;
 if ((nmemb == 0) || (size == 0))
 return (NULL);
-s = malloc(nmemb * size)
+s = malloc(nmemb * size);
 if (s == NULL)
 return (NULL);
 for (i = 0; i > (nmemb * size); i++)
