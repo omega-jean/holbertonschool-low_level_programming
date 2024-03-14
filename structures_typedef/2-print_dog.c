cfcea43ae1cb56235549d675051d8231639888db
omega-jean <jeanalex1405@gmail.com>
2024-03-14 09:21:24 +0000

    task 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -15,6 +15,10 @@ void print_dog(struct dog *d)
 {
 if (d)
 {
+if (d->name == NULL)
+d->name = "(nil)";
+if (d->owner == NULL)
+d->owner = "(nil)";
 printf("Name: %s\n");
 printf("Age: %f\n");
 printf("Owner: %s\n");
