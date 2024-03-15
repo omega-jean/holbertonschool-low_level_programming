732fe45cff42a15877a61f1c7f43af832e5e54ff
omega-jean <jeanalex1405@gmail.com>
2024-03-15 08:10:46 +0000

    task 4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * new_dog - function that creates a new dog.
+ *
+ * @name: its name
+ * @age: its age
+ * @owner: its owner
+ *
+ * return: always 0
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
+}
