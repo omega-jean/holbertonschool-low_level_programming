871933b0338c926b7f38ed3229a719368fe93395
omega-jean <jeanalex1405@gmail.com>
2024-03-15 10:33:00 +0000

    task 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -15,5 +15,17 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-
+int i = 0;
+int j = 0;
+int k;
+while (name[i] != '\0')
+i++;
+while (owner[j] != '\0')
+j++;
+dog_t *new_dog = (dog_t *)malloc(sizeof(dog_t));
+if (new_dog == NULL)
+return (NULL);
+new_dog->name = (char *) malloc(i + 1);
+new_dog->owner = (char *) malloc(j + 1);
+if (new_dog->name == NULL
 }
