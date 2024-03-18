e1f6b68541b0774205a45c018d938b89731d152a
omega-jean <jeanalex1405@gmail.com>
2024-03-18 10:06:18 +0000

    task 0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -3,11 +3,13 @@
 /**
  * print_name - function that prints a name.
  *
- * @name:
- * @f:
+ * @name: its name
+ * @f: its f
  *
  * Return: Always 0.
  */
 
 void print_name(char *name, void (*f)(char *))
-
+{
+f(name);
+}
