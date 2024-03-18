9e4df8332d49184d3def80ec34f617a9bd817c8b
omega-jean <jeanalex1405@gmail.com>
2024-03-18 13:54:44 +0000

    task 2

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -13,10 +13,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 int i;
 int res = 0;
-if (res == 0 || size < 0)
+if (size < 0)
 {
 return (-1);
 }
 for (i =0; i < size; i++)
 res = (cmp(array[i]));
+if (res = 0)
+return (-1);
 }
