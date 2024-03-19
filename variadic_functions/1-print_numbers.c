575f87587997462aef09ac7a8445414cdfeabc22
omega-jean <jeanalex1405@gmail.com>
2024-03-19 10:25:46 +0000

    task 1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "variadic_functions.h"
+#include <stdarg.h>
+
+/**
+ * sprint_numbers - function that prints numbers, followed by a new line.
+ *
+ * @separator
+ * @n: its a number
+ *
+ * Return: Always 0.
+ */
+
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+unsigned int i;
+va_list args;
+va_start(args, n);
+for (i = 0; i < n; i++)
+{
+int num = va_arg(args, int);
+printf ("%d", num);
+if (i < n -1)
+(
+
+va_end(args);
+printf('\n');
+}
