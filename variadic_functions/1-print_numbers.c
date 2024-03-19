42f962c5f1f41159dc9397b0504760d052f36a01
omega-jean <jeanalex1405@gmail.com>
2024-03-19 10:54:23 +0000

    task 1

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -6,7 +6,7 @@
 /**
  * sprint_numbers - function that prints numbers, followed by a new line.
  *
- * @separator
+ * @separator: its a separator
  * @n: its a number
  *
  * Return: Always 0.
@@ -18,15 +18,15 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 unsigned int i;
 va_list args;
 va_start(args, n);
-for (i = 0; i < n; i++)
+for(i = 0; i < n; i++)
 {
 int num = va_arg(args, int);
 printf ("%d", num);
-if (i < n -1 && separator != NULL)
+if (i < n - 1 && separator != NULL)
 {
-printf ("%s", separator);
+printf("%s", separator);
 }
 }
 va_end(args);
-printf ("\n");
+printf("\n");
 }
