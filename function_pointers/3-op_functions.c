b431bf6c7d40b513f49291111b7c0cf05cfee4ef
omega-jean <jeanalex1405@gmail.com>
2024-03-19 13:32:41 +0000

    op

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -54,6 +54,11 @@ return (a * b);
 
 int op_div(int a, int b)
 {
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
 return (a / b);
 }
 
@@ -68,5 +73,10 @@ return (a / b);
 
 int op_mod(int a, int b)
 {
+if (b == 0)
+{
+printf("Error\n");
+exit(100);
+}
 return (a % b);
 }
