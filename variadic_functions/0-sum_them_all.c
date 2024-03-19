d301034d435d7217033bef72794587739b6383cd
omega-jean <jeanalex1405@gmail.com>
2024-03-19 09:05:51 +0000

    task 0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -16,11 +16,11 @@ if (n == 0)
 return (0);
 int sum = 0;
 va_list args;
-va_start (args, n);
+va_start(args, n);
 for (unsigned int i = 0; i < n; i++)
 {
 sum += args(args, int);
 }
-va_end (args);
+va_end(args);
 return (sum);
 }
