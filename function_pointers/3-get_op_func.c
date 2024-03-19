50c21f8467933d9fbab9dbd57c545e10070c41f5
omega-jean <jeanalex1405@gmail.com>
2024-03-19 08:35:05 +0000

    get

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "function_pointers.h"
+#include <stdlib.h>
+
+/**
+ * get_op_func - This file should contain the function that selects the correct
+ * function to perform the operation asked by the user.
+ *
+ * @s: its s
+ *
+ * Return: always 0
+ */
+
+int (*get_op_func(char *s))(int, int)
+op_t ops[] = {
+{"+", op_add},
+{"-", op_sub},
+{"*", op_mul},
+{"/", op_div},
+{"%", op_mod},
+{NULL, NULL}
+};
+int i;
