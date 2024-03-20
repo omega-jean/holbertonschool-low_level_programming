aad72bf57375210beb32e10c45fae2fe111865a4
omega-jean <jeanalex1405@gmail.com>
2024-03-20 09:49:03 +0000

    task 2

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -13,7 +13,7 @@
  */
 
 
-void print_strings(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...)
 {
 unsigned int i;
 va_list args;
