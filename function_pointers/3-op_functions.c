8a84009ddf36b11c1b5cef23b0abbfd31f0cb708
omega-jean <jeanalex1405@gmail.com>
2024-03-20 09:20:31 +0000

    op

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+#include "3-calc.h"
+
+/**
+ * op_add - function that add the number
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result
+ */
+
+int op_add(int a, int b)
+{
+return (a + b);
+}
+
+/**
+ * op_sub - function that sub the number
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result
+ */
+
+int op_sub(int a, int b)
+{
+return (a - b);
+}
+
+/**
+ * op_mul - function that mult the number
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result
+ */
+
+int op_mul(int a, int b)
+{
+return (a * b);
+}
+
+/**
+ * op_div - function that div the number
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result
+ */
+
+int op_div(int a, int b)
+{
+return (a / b);
+}
+
+/**
+ * op_mod - function that mod the number
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: result
+ */
+
+int op_mod(int a, int b)
+{
+return (a % b);
+}
