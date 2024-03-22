368e2f37828a7e5e5db3922cd6b914003bf111bd
omega-jean <jeanalex1405@gmail.com>
2024-03-22 13:49:02 +0000

    all

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,11 @@
 #ifndef variadic_functions_h
 #define variadic_functions_h
 
+typedef struct {
+char pr;
+void (*f)(va_list);
+} pr_t;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
