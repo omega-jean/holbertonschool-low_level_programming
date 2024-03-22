14ca7b3c214e47c797eaabac54973c2ca8da81fe
omega-jean <jeanalex1405@gmail.com>
2024-03-22 13:10:39 +0000

    task 3

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -5,6 +5,61 @@
 
 
 /**
+ * print_char - print a character
+ *
+ * @ap: character.
+ */
+
+void print_char(va_list ap)
+{
+printf("%c", va_arg(ap, int));
+}
+
+
+/**
+ * print_int - print a integer
+ *
+ * @ap: character.
+ */
+
+void print_int(va_list ap)
+{
+printf("%i", va_arg(ap, int));
+}
+
+
+/**
+ * print_float - print a float
+ *
+ * @ap: character.
+ */
+
+void print_float(va_list ap)
+{
+printf("%f", va_arg(ap, int));
+}
+
+
+/**
+ * print_string - print a string
+ *
+ * @ap: character.
+ *
+ * return: always 0
+ */
+
+void print_string(va_list ap)
+{
+if (s == NULL)
+{
+printf("(nil)");
+return;
+}
+printf("%s", va-arg(ap, int));
+}
+
+
+/**
  * print_all - function that prints anything.
  *
  * @format: is a list of types of arguments passed
@@ -16,42 +71,4 @@
 
 void print_all(const char * const format, ...)
 {
-va_list args;
-char c;
-int i;
-float f;
-char *s;
-const char *ptr = format; /* declares a constant pointer to a character (const char *) named ptr and initializes it with the address of the first character in the format string.*/
-va_start(args, format);
-while (*ptr)
-{
-if (*ptr == 'c')
-{
-c = va_arg(args, int); /* Retrieves the next argument from the list of args variable arguments as an integer (int) and assigns this value to variable c. */
-printf("%c", c);
-}
-else if (*ptr == 'i')
-{
-i = va_arg(args, int);
-printf("%i", i);
-}
-else if (*ptr == 'f')
-{
-f = va_arg(args, double);
-printf("%f", f);
-}
-else if (*ptr == 's')
-{
-s = va_arg(args, char *);
-if (s == NULL)
-printf("(nil)");
-else
-printf("%s", s);
-}
-ptr++;
-if (*ptr)
-printf(", ");
-}
-va_end(args);
-printf("\n");
 }
