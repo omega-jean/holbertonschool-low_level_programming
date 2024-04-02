3137327210f138ba367ebe68a4ecd7fb80c5dc93
omega-jean <jeanalex1405@gmail.com>
2024-04-02 08:13:50 +0000

    add description

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * print_len - Function that returns the number of elements in a linked
+ * list_len - Function that returns the number of elements in a linked
  *
  * @h: Pointer to the head of the list.
  *
- * Return: ...
+ * Return: The number of nodes.
  */
 
 size_t list_len(const list_t *h)
