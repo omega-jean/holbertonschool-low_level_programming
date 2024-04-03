e72fab09c78a3bf72560744b337808aa17031b84
omega-jean <jeanalex1405@gmail.com>
2024-04-03 09:52:54 +0000

    add function

---
+++ doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - function that frees a dlistint_t list.
+ *
+ * @head: pointer
+ *
+ * Return: The number of the node
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp);
+	}
+}
