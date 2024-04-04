685c4ef1a9484fbf66f9248661b59231f80a65e5
omega-jean <jeanalex1405@gmail.com>
2024-04-04 13:24:57 +0000

    fix code

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -29,7 +29,7 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 
 	if (idx == n)
 	{
-		new_next-> = *h;
+		new_node->next = *h;
 	if (h != NULL)
 		(*h)->prev = new_node;
         *h = new_node;
