3322a9892db535008837beb867cdb669e36a292e
omega-jean <jeanalex1405@gmail.com>
2024-04-07 04:46:29 +0000

    fix code

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -7,24 +7,24 @@
  * Return: 1 or -1;
  */
 
-int create_custom_file(const char *custom_filename, char *custom_text_content)
+int create_file(const char *filename, char *text_content)
 {
 	int custom_fd, custom_len, custom_w;
 
-	if (custom_filename == NULL)
+	if (filename == NULL)
 		return (-1);
 
-	custom_fd = open(custom_filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
+	custom_fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 00600);
 
 	if (custom_fd == -1)
 	{
 		return (-1);
 	}
-	if (custom_text_content != NULL)
+	if (text_content != NULL)
 	{
-		for (custom_len = 0; custom_text_content[custom_len] != '\0'; custom_len++)
+		for (custom_len = 0; text_content[custom_len] != '\0'; custom_len++)
 			;
-		custom_w = write(custom_fd, custom_text_content, custom_len);
+		custom_w = write(custom_fd, text_content, custom_len);
 	}
 	if (custom_w == -1)
 	{
