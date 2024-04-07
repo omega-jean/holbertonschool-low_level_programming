cbda60d62e66c2a75f478d078833a36a5fdb26ac
omega-jean <jeanalex1405@gmail.com>
2024-04-07 04:53:01 +0000

    fix betty

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -2,29 +2,28 @@
 
 /**
  * append_text_to_file - appand a text to the end of file
- 
  * @filename: filename
  * @text_content: text content of the file
- 
  * Return: 1 or -1
  */
 
+int append_text_to_file(const char *filename, char *text_content)
 {
-    int custom_fd, custom_len, custom_w;
+	int custom_fd, custom_len, custom_w;
 
-    if (custom_filename == NULL)
-        return (-1);
-    custom_fd = open(custom_filename, O_APPEND | O_WRONLY);
-    if (custom_fd == -1)
-        return (-1);
-    if (custom_text_content != NULL)
-    {
-        for (custom_len = 0; custom_text_content[custom_len] != '\0'; custom_len++)
-            ;
-        custom_w = write(custom_fd, custom_text_content, custom_len);
-        if (custom_w == -1)
-            return (-1);
-    }
-    close(custom_fd);
-    return (1);
+	if (filename == NULL)
+		return (-1);
+	custom_fd = open(filename, O_APPEND | O_WRONLY);
+	if (custom_fd == -1)
+		return (-1);
+	if (text_content != NULL)
+	{
+		for (custom_len = 0; text_content[custom_len] != '\0'; custom_len++)
+			;
+		custom_w = write(custom_fd, text_content, custom_len);
+		if (custom_w == -1)
+			return (-1);
+	}
+	close(custom_fd);
+	return (1);
 }
